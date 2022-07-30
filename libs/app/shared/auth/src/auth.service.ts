8d3a70aef89d4d6950383e1633cb8d988ee31736
David Cho <david.hoontime@gmail.com>
2022-07-30 22:42:39 +0200

    chore: add comment for understanding

--- libs/app/shared/auth/src/auth.service.ts
+++ libs/app/shared/auth/src/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../../users/src/users.service';
 
--- libs/app/shared/auth/src/jwt.strategy.ts
+++ libs/app/shared/auth/src/jwt.strategy.ts
@@ -12,6 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
+    console.log('[JwtStrategy - validate]');
     return {
       userId: payload.sub,
       username: payload.username,
